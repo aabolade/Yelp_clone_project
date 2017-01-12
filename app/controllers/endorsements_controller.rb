86a9c28193d87d3d2f67ce3156d6887643703b80
Pea M. Tyczynska <CrystalPea@github.com>
2017-01-12 16:40:07 +0000

    Endorsements ready

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,6 +1,6 @@
 class EndorsementsController < ApplicationController
 
-  def create
+  def new
     @review = Review.find(params[:review_id])
     @review.endorsements.create
     redirect_to restaurants_path
